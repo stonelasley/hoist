10e7c999122c5ef739c1e6f263077caa8b0a341a
Stone C. Lasley <stone@lasley.io>
2026-02-14 15:29:52 -0700

    fix(web): skip db init during NSwag build step

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -25,7 +25,7 @@ if (builder.Environment.IsDevelopment())
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() && Environment.GetEnvironmentVariable("SKIP_DB_INIT") != "true")
 {
     await app.InitialiseDatabaseAsync();
 }
